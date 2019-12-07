21159d05f9e55794a09f59fa6299d53967da98b2
Radoslaw <rakodoza@gmail.com>
2019-12-07 18:59:47 +0100

    Added Create and Details to PagesController

--- WebShop/Areas/Admin/Controllers/PagesController.cs
+++ WebShop/Areas/Admin/Controllers/PagesController.cs
@@ -21,6 +21,7 @@ namespace WebShop.Areas.Admin.Controllers
 
         // IActionResult can return more on less anything
         // if you want to be more specific use for example IRedirectResult
+        // GET /admin/pages
         public async Task<IActionResult> Index()
         {
             IQueryable<Page> pages = from p in _context.Pages orderby p.Sorting select p;
@@ -28,6 +29,45 @@ namespace WebShop.Areas.Admin.Controllers
 
             // we do not specify the view name here so by default it will look for the View with the method name which is "Index" in this case
             return View(pageList);
+        }
+
+        // GET /admin/pages/details/6
+        public async Task<IActionResult> Details(int id)
+        {
+            Page page = await _context.Pages.FirstOrDefaultAsync(x => x.Id == id);
+            if(page == null)
+                return NotFound();
+
+            return View(page);
+        }
+
+        // GET /admin/pages/create
+        public IActionResult Create() => View();
+
+        // POST /admin/pages/details/6
+        [HttpPost] // if we don't specify the request attribute it is GET by default
+        public async Task<IActionResult> Create(Page page)
+        {
+            // model binding - it is a way to pass data to methods
+            if(ModelState.IsValid)
+            {
+                page.Slug = page.Title.ToLower().Replace(" ", "-");
+                page.Sorting = 100;
+
+                var slug = await _context.Pages.FirstOrDefaultAsync(x => x.Slug == page.Slug);
+                if(slug != null)
+                {
+                    ModelState.AddModelError("", "The title already exists.");
+                    return View(page);
+                }
+
+                _context.Add(page);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(page);
         }
     }
 }
--- WebShop/Models/Page.cs
+++ WebShop/Models/Page.cs
@@ -10,10 +10,12 @@ namespace WebShop.Models
     {
         public int Id { get; set; } // this is going to be automatically recognized as the primary key
         [Required] // to make the column not nullable in the db
+        //[Display(Name = "Fruit")] // it will be displayed in the view if we have a @Html.DsiplayNameFor method used
+        [MinLength(2, ErrorMessage = "Minimum length is 2")] // custom error message
         public string Title { get; set; }
         [Required]
         public string Slug { get; set; }
-        [Required]
+        [Required, MinLength(4, ErrorMessage = "Minimum length is 4")] // it is possible to add more than one attributes in one line
         public string Content { get; set; }
         public int Sorting { get; set; }
     }
